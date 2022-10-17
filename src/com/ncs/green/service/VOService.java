75bb6bcab42f24933c6277cbcc028b8c3b57bc9a
JOO <JOO@JOO>
2022-10-17 12:54:32 +0900

    Test01-이클립스

---
+++ src/com/ncs/green/service/VOService.java
@@ -0,0 +1,14 @@
+package com.ncs.green.service;
+
+import com.ncs.green.vo.MemberVO;
+
+public class VOService {
+	private MemberVO memberVO;
+
+	public MemberVO getMemberVO() {	return memberVO;	}
+
+	public void setMemberVO(MemberVO memberVO) {	this.memberVO = memberVO;	}
+	
+	
+
+} //class
